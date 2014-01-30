5a678a2229a54cf625b8bb50562f5c3a2b063b37
nineties <nineties48@gmail.com>
2014-01-31 00:07:31 +0900

    Update documents.

--- data/ex18-5-4.py
+++ data/ex18-5-4.py
@@ -9,9 +9,15 @@ y = data[:,1]
 N = len(x)
 
 k = 9       # パラメータの数x^kの項まで使う
-l = 1       # sigma^2/sigma_0^2
+
+# L = sigma^2 * Sigma_0^{-1}
+L = np.diag([10**i for i in range(k+1)])
 
 # 回帰分析
 G = np.array([x**i for i in range(k+1)]).T
-a = LA.solve(G.T.dot(G) + l*np.identity(k+1), G.T.dot(y))
-print a
+a = LA.solve(G.T.dot(G) + L, G.T.dot(y))
+
+print "{0}".format(a[0]),
+for i in range(1, k+1):
+    print " + {0}*x**{1}".format(a[i], i),
+print ""
